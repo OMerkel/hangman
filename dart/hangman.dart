9437f905a66c733592fc4dcc500ddc32acd7c0a9
Oliver Merkel <Merkel.Oliver@web.de>
2019-05-03 01:40:04 +0200

    Update hangman.dart

--- dart/hangman.dart
+++ dart/hangman.dart
@@ -1,6 +1,30 @@
+// Copyright (c) 2019, Oliver Merkel.
+// Please see the AUTHORS file for details.
+// All rights reserved.
+//
+// Use of this code is governed by a
+// MIT license that can be found in the LICENSE file.
+
 import 'dart:io';
 import 'dart:convert';
 import 'dart:math';
+
+bool playAgain() {
+  var yes = [ 'y', 'yes', 'sure' ];
+  var no = [ 'n', 'no', 'nope' ];
+  var isYes = false;
+  var isNo = false;
+  while (!isYes && !isNo) {
+    print('Play again? ');
+    var answer = stdin.readLineSync().toLowerCase();
+    isYes = yes.contains(answer);
+    isNo = no.contains(answer);
+    if (!isYes && !isNo) {
+      print('Sorry? Please answer with yes or no...');
+    }
+  }
+  return isYes;
+}
 
 String getWord() {
   var rand = Random();
@@ -35,50 +59,55 @@ bool isalpha(c) {
 
 void main() {
   var maxFalseAttempts = 5;
-  var falseAttempts = 0;
-  var endOfGame = false;
-  var word = getWord();
-  var guesses = '';
-  print('Guess my secret word with a maximum' +
-    'of ${maxFalseAttempts} false attempts.');
-  while (!endOfGame) {
-    var test = getResult(word, guesses);
-    print(test['result']);
-    if (test['incorrect'] == 0) {
-      endOfGame = true;
-      print('You won!');
+  var keepPlaying = true;
+  while (keepPlaying) {
+    var falseAttempts = 0;
+    var endOfGame = false;
+    var word = getWord();
+    var guesses = '';
+    print('Guess my secret word with a maximum ' +
+      'of ${maxFalseAttempts} false attempts.');
+    while (!endOfGame) {
+      var test = getResult(word, guesses);
+      print(test['result']);
+      if (test['incorrect'] == 0) {
+        endOfGame = true;
+        print('You won!');
+      }
+      else{
+        print('Guess a character: ');
+        var guess = stdin.readLineSync().toLowerCase();
+        if (guess.length != 1) {
+          print('Type a single character only! Try again...');
+          continue;
+        }
+        if (!isalpha(guess)) {
+          print('Type a alphabetic character! Try again...');
+          continue;
+        }
+        if (guesses.contains(guess)) {
+          print('You already guessed this character! Try again...');
+          continue;
+        }
+        guesses += guess;
+        if (!word.contains(guess)) {
+          falseAttempts++;
+          print('Wrong.');
+          if (falseAttempts == 1) {
+            print('You have one false attempt.');
+          }
+          else {
+            print('You have ${falseAttempts} false attempts.');
+          }
+          if (falseAttempts > maxFalseAttempts) {
+            endOfGame = true;
+            print('You lost!');
+            print('The secret word has been ' + word.toUpperCase());
+          }
+        }
+      }
     }
-    else{
-      print('Guess a character: ');
-      var guess = stdin.readLineSync().toLowerCase();
-      if (guess.length != 1) {
-        print('Type a single character only! Try again...');
-        continue;
-      }
-      if (!isalpha(guess)) {
-        print('Type a alphabetic character! Try again...');
-        continue;
-      }
-      if (guesses.contains(guess)) {
-        print('You already guessed this character! Try again...');
-        continue;
-      }
-      guesses += guess;
-      if (!word.contains(guess)) {
-        falseAttempts++;
-        print('Wrong.');
-        if (falseAttempts == 1) {
-          print('You have one false attempt.');
-        }
-        else {
-          print('You have ${falseAttempts} false attempts.');
-        }
-        if (falseAttempts > maxFalseAttempts) {
-          endOfGame = true;
-          print('You lost!');
-          print('The secret word has been ' + word.toUpperCase());
-        }
-      }
-    }
+    keepPlaying = playAgain();
   }
+  print('Bye.');
 }
